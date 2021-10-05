a111067132fbe2c3226a8167a898e2cc7ea54343
mariaang7 <78777007+mariaang7@users.noreply.github.com>
2021-10-05 16:53:33 -0700

    Update hash_round.v

--- hash_round.v
+++ hash_round.v
@@ -1,15 +1,39 @@
-module hash_round (
-   
+module hash_round #(parameter ROUND) (
+	input wire [7:0] in_byte,
+	input wire [31:0] in_state,
+	output wire [7:0] out_state
 );
    
 	// Declarations
+	wire [7:0] d;
+	wire [7:0] c;
+	wire [7:0] b;
+	wire [7:0] a;
+	wire [7:0] mixed_a = (mix(d, c, b) + a) + in_byte;
+	wire [7:0] rotated_mixed_a;
 	
 	// State splitting
+	assign {d, c, b, a} = in_state;
 	
 	// Mix function
-	
+	function [7:0] mix;
+		input [7:0] d, c, b;
+		input [2:0] ROUND;
+		output [7:0] mixed;
+		always @(*) begin
+			if (ROUND == 3'b000 || ROUND == 3'b001 || ROUND == 3'b010)
+				mixed = (c & b) | (~b & d);
+			else if (ROUND == 3'b011 || ROUND == 3'b100)
+				mixed = (c & b) | (b & d) | (d & c);
+			else if (ROUND == 3'b101 || ROUND == 3'b110 || ROUND == 3'b111)
+				mixed = c ^ b ^ d;
+		end
+	endfunction
+					
 	// Rotator
+	rotator #(WIDTH = 8) rotator_mix (.in(mixed_a), .direction(1'b1), .distance(ROUND), .out(rotated_mixed_a));
 	
 	// Output state assignment
+	assign out_state = {c, b, rotated_mixed_a, d};
 
 endmodule
