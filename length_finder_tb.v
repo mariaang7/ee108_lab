3d562ce9241076c55961eacde8b87f20eb6f9c67
mariaang7 <78777007+mariaang7@users.noreply.github.com>
2021-10-06 14:38:09 -0700

    Update length_finder_tb.v

--- length_finder_tb.v
+++ length_finder_tb.v
@@ -1,6 +1,7 @@
 module length_finder_tb ();
 
    reg [63:0] in;
+   reg [3:0] expected;
    wire [3:0] len;
    
    length_finder dut (
@@ -12,38 +13,38 @@ module length_finder_tb ();
       in = 64'hAABBCCDDEEFFAA00;
       expected = 4'b0111;
       #5
-      $display("%b -> %d, expected %d", in, out, expected);
+      $display("%b -> %d, expected %d", in,len, expected);
       in = 64'hAABBCCDDEEFFAA99;
       expected = 4'b1000;
       #5
-      $display("%b -> %d, expected %d", in, out, expected);
+      $display("%b -> %d, expected %d", in, len, expected);
       in = 64'hAABBCCDDEEFF00AA;
       expected = 4'b0110;
       #5
-      $display("%b -> %d, expected %d", in, out, expected);
+      $display("%b -> %d, expected %d", in, len, expected);
       in = 64'hAABBCCDDEE00FFAA;
       expected = 4'b0101;
       #5
-      $display("%b -> %d, expected %d", in, out, expected);
+      $display("%b -> %d, expected %d", in, len, expected);
       in = 64'hAABBCC00EE00FFAA;
       expected = 4'b0011;
       #5
-      $display("%b -> %d, expected %d", in, out, expected);
+      $display("%b -> %d, expected %d", in, len, expected);
       in = 64'h00BBCCDDEE44FFAA;
       expected = 4'b0000;
       #5
-      $display("%b -> %d, expected %d", in, out, expected);
+      $display("%b -> %d, expected %d", in, len, expected);
       in = 64'h00BBCC00EE44FFAA;
       expected = 4'b0000;
       #5
-      $display("%b -> %d, expected %d", in, out, expected);
+      $display("%b -> %d, expected %d", in, len, expected);
       in = 64'h44BBC00DEE44FFAA;
       expected = 4'b1000;
       #5
-      $display("%b -> %d, expected %d", in, out, expected);
+      $display("%b -> %d, expected %d", in, len, expected);
       in = 0;
       expected = 4'b1000;
-      $display("%b -> %d, expected %d", in, out, expected);
+      $display("%b -> %d, expected %d", in, len, expected);
    end
    
 endmodule
