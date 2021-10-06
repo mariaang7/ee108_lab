9dcb859b1da9e54f98e3d6e08a48094f63938aae
cate2002 <91645354+cate2002@users.noreply.github.com>
2021-10-05 23:40:22 -0700

    Update hasher.v

--- hasher.v
+++ hasher.v
@@ -4,12 +4,18 @@ module hasher #(parameter ROUND) (
   output wire [31:0] hash
 );
   
-  wire data_len = 
+  wire dir;
+  wire final_state [31:0];
+  wire
+  
+  
   
   always @(*) begin
-    
-  end 
-  
+    case (data_len)
+	      4'b0000 : dir = 1'b0;
+        default : dir = 1'b1
+	    endcase
+	 end
   
   rotator #(WIDTH=32) final_rotator (.in(mixed_a), .direction(1'b1), .distance(ROUND), .out(rotated_mixed_a));
   
