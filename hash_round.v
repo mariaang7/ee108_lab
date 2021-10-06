90f27e1b96a67b5a68f166d9426ed7edb3c83e00
cate2002 <91645354+cate2002@users.noreply.github.com>
2021-10-06 11:41:06 -0700

    Update hash_round.v

--- hash_round.v
+++ hash_round.v
@@ -1,4 +1,4 @@
-module hash_round #(parameter ROUND) (
+module hash_round #(parameter ROUND=8) (
 	input wire [7:0] in_byte,
 	input wire [31:0] in_state,
 	output wire [7:0] out_state
@@ -9,29 +9,31 @@ module hash_round #(parameter ROUND) (
 	wire [7:0] c;
 	wire [7:0] b;
 	wire [7:0] a;
-	wire [7:0] mixed_a = (mix(d, c, b) + a) + in_byte;
+	
+	wire [7:0] mixed_a = (mix + a) + in_byte;
 	wire [7:0] rotated_mixed_a;
 	
 	// State splitting
 	assign {d, c, b, a} = in_state;
 	
 	// Mix function
-	function [7:0] mix;
-		input [7:0] d, c, b;
-		input [2:0] ROUND;
-		output [7:0] mixed;
+	reg [7:0] mix;
+		
+	
 		always @(*) begin
 			if (ROUND == 3'b000 || ROUND == 3'b001 || ROUND == 3'b010)
-				mixed = (c & b) | (~b & d);
+				mix = (c & b) | (~b & d);
 			else if (ROUND == 3'b011 || ROUND == 3'b100)
-				mixed = (c & b) | (b & d) | (d & c);
+				mix = (c & b) | (b & d) | (d & c);
 			else if (ROUND == 3'b101 || ROUND == 3'b110 || ROUND == 3'b111)
-				mixed = c ^ b ^ d;
+				mix = c ^ b ^ d;
+			
+		
 		end
-	endfunction
+
 					
 	// Rotator
-	rotator #(WIDTH = 8) rotator_mix (.in(mixed_a), .direction(1'b1), .distance(ROUND), .out(rotated_mixed_a));
+	rotator #(8) rotator_mix (.in(mixed_a), .direction(1'b1), .distance(ROUND), .out(rotated_mixed_a));
 	
 	// Output state assignment
 	assign out_state = {c, b, rotated_mixed_a, d};
