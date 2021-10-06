6f2185ee50a20b6232586e2205a438683665b372
mariaang7 <78777007+mariaang7@users.noreply.github.com>
2021-10-06 13:57:58 -0700

    Update length_finder_tb.v

--- length_finder_tb.v
+++ length_finder_tb.v
@@ -10,30 +10,39 @@ module length_finder_tb ();
    
    initial begin
       in = 64'hAABBCCDDEEFFAA00;
+      expected = 4'b0111;
       #5
       $display("%b -> %d, expected %d", in, out, expected);
       in = 64'hAABBCCDDEEFFAA99;
+      expected = 4'b1000;
       #5
       $display("%b -> %d, expected %d", in, out, expected);
       in = 64'hAABBCCDDEEFF00AA;
+      expected = 4'b0110;
       #5
       $display("%b -> %d, expected %d", in, out, expected);
       in = 64'hAABBCCDDEE00FFAA;
+      expected = 4'b0101;
       #5
       $display("%b -> %d, expected %d", in, out, expected);
       in = 64'hAABBCC00EE00FFAA;
+      expected = 4'b0011;
       #5
       $display("%b -> %d, expected %d", in, out, expected);
       in = 64'h00BBCCDDEE44FFAA;
+      expected = 4'b0000;
       #5
       $display("%b -> %d, expected %d", in, out, expected);
       in = 64'h00BBCC00EE44FFAA;
+      expected = 4'b0000;
       #5
       $display("%b -> %d, expected %d", in, out, expected);
       in = 64'h44BBC00DEE44FFAA;
+      expected = 4'b0000;
       #5
       $display("%b -> %d, expected %d", in, out, expected);
       in = 0;
+      expected = 4'b0000;
       $display("%b -> %d, expected %d", in, out, expected);
    end
    
