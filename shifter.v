511f71977a81aa13b38d68f03114e0cf382fc290
mariaang7 <78777007+mariaang7@users.noreply.github.com>
2021-10-10 20:18:04 -0700

    Update shifter.v

--- shifter.v
+++ shifter.v
@@ -1,9 +1,15 @@
 module shifter (
-
+  input wire [4:0] in,
+  input wire [2:0] distance,
+  input wire direction,
+  output reg [4:0] out
 );
 
-// your implementation goes here!
-
-
+  always @(*) begin
+    case (in)
+      0'b0: out = in << distance;
+      0'b1: out = in >> distance;
+    endcase
+  end
 
 endmodule
