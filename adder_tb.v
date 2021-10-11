563dfdaa9eddc6507546a956fd39cccfc45cb88f
polys18 <75305203+polys18@users.noreply.github.com>
2021-10-10 20:48:56 -0700

    Update adder_tb.v

--- adder_tb.v
+++ adder_tb.v
@@ -1,8 +1,40 @@
-module adder_tb (
+module adder_tb ();
+    reg [4:0] a;
+    reg [4:0] b;
+    reg [4:0] expected;
+    reg [4:0] sum;
+    reg cout;
+    
+    adder add (.a(a), .b(b), .sum(sum), .cout(cout));
 
-);
 
-// your implementation goes here!
+initial begin
+
+    a = 5'b00001;
+    b = 5'b00010;
+    expected = 5'b00011;
+    $display("%d, expected %d, cout=%d expected_cout=0", sum , expected, cout);
+    
+    
+    a = 5'b10000;
+    b = 5'b00001;
+    expected = 5'b10001;
+    $display("%d, expected %d, cout=%d expected_cout=0", sum , expected, cout);
+    
+    
+    a = 5'b00000;
+    b = 5'b00000;
+    expected = 5'b00000;
+    $display("%d, expected %d, cout=%d expected_cout=0", sum , expected, cout);
+    
+    
+    a = 5'b11111;
+    b = 5'b11111;
+    expected = 5'b11110;
+    $display("%d, expected %d, cout=%d expected_cout=1", sum , expected, cout);
+    
+
+end
 
 
 
