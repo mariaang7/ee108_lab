b8ead9003a9de41c9c98a744f8f327d5e632d163
cate2002 <91645354+cate2002@users.noreply.github.com>
2021-10-10 20:03:10 -0700

    Update big_number_first.v

--- big_number_first.v
+++ big_number_first.v
@@ -1,9 +1,11 @@
-module big_number_first (
+module big_number_first ( input wire [7:0] aIn,
+                         input wire [7:0] bIn,
+                         input wire [7:0] aOut, 
+                         input wire [7:0] bIn
 
 );
-
-// your implementation goes here!
-
-
-
+  
+  assign aOut = aIn >= bIn ? aIn:bIn;
+  assign bOut = bIn >= aIn ? aIn:bIn;
+  
 endmodule
