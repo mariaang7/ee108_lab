38028ce66f8eab5e92bc751b78b1df79a8a8f8b1
cate2002 <91645354+cate2002@users.noreply.github.com>
2021-10-10 20:30:09 -0700

    Update big_number_first_tb.v

--- big_number_first_tb.v
+++ big_number_first_tb.v
@@ -1,8 +1,42 @@
-module big_number_first_tb (
+module big_number_first_tb (input wire [7:0] aIn,
+                            input wire [7:0] bIn, 
+                            output wire [7:0] aOut,
+                            output wire [7:0] bOut
 
 );
 
-// your implementation goes here!
+initial begin
+        // Basic test case #1
+        in = 8'b00000110;
+        expected = 8'b00000010;
+        #5
+        $display("%b -> %b, expected %b", in, out, expected);
+
+        // Basic test case #2
+        in = 8'b00100000;
+        expected = 8'b00100000;
+        #5
+        $display("%b -> %b, expected %b", in, out, expected);
+
+        // Basic test case #3
+        in = 8'b01010100;
+        expected = 8'b00000100;
+        #5
+        $display("%b -> %b, expected %b", in, out, expected);
+        
+        // Basic test case #4
+        in = 8'b01110000;
+        expected = 8'b00010000;
+        #5
+        $display("%b -> %b, expected %b", in, out, expected);
+        
+        // Basic test case #5
+        in = 8'b10000000;
+        expected = 8'b10000000;
+        #5
+        $display("%b -> %b, expected %b", in, out, expected);
+
+    end
 
 
 
