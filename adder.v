03d6b26a217eb81cd2f6a7c2378f1c660ba5c3c2
cate2002 <91645354+cate2002@users.noreply.github.com>
2021-10-10 20:27:50 -0700

    Update adder.v

--- adder.v
+++ adder.v
@@ -1,21 +1,20 @@
 module adder (
     input wire [4:0] a,
     input wire [4:0] b,
-    output wire [4:0] out,
+    output wire [4:0] sum,
     output reg cout
 );
 
 wire [6:0] result;
 wire [4:0] actual;
 assign result = a + b;
-assign actual = a + b;
+assign sum = a + b;
 always @(*) begin
-    if (result != actual) begin
+    if (result != sum) begin
         cout = 1'b1;
     end else begin
         cout = 1'b0;
     end
-end
-
-
+end 
+    
 endmodule
