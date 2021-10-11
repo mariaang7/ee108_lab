6f40214237c8df7c20864079f678733fd5712c65
mariaang7 <78777007+mariaang7@users.noreply.github.com>
2021-10-10 20:30:10 -0700

    Update shifter_tb.v

--- shifter_tb.v
+++ shifter_tb.v
@@ -1,8 +1,16 @@
-module shifter_tb (
+module shifter_tb ();
+  reg [4:0] in;
+  reg [2:0] distance;
+  reg direction;
+  reg expected;
+  wire [4:0] out;
+  shifter dut (
+    .in(in), 
+    .distance(distance),
+    .direction(direction),
+    .out(out)
+  );
 
-);
-
-// your implementation goes here!
 
 
 
