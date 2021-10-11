6633f2eaaaacfc37064bb5c66f3bbcbbf8992b57
polys18 <75305203+polys18@users.noreply.github.com>
2021-10-10 20:19:30 -0700

    Update adder.v

--- adder.v
+++ adder.v
@@ -1,9 +1,21 @@
 module adder (
-
+    input wire [4:0] a,
+    input wire [4:0] b,
+    output wire [4:0] out,
+    output reg cout
 );
 
-// your implementation goes here!
-
+wire [6:0] result;
+wire [4:0] actual;
+assign result = a + b;
+assign actual = a + b;
+always @(*) begin
+    if (result != actual) begin
+        cout = 1'b1;
+    end else begin
+        cout = 1'b0;
+    end
+end
 
 
 endmodule
