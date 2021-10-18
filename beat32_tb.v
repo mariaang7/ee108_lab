e328be69a697c4a442ec8fa5e3c80fc9fe4af9ce
polys18 <75305203+polys18@users.noreply.github.com>
2021-10-18 13:17:07 -0700

    Update beat32_tb.v

--- beat32_tb.v
+++ beat32_tb.v
@@ -1,5 +1,6 @@
 module beat32_tb ();
-  reg clock, reset, count;
+  reg clock, reset;
+  wire count;
   beat32 dud (.clock(clock), .reset(reset), .count(count));
   initial
     forever
@@ -12,7 +13,9 @@ module beat32_tb ();
     #10 reset = 0 ;    // remove reset
     #30
     $display("expected: 0 ---> %d", count);
-    #20
+    #40
+    $display("expected: 1 ---> %d", count);
+    #70
     $display("expected: 1 ---> %d", count);
     #80   // 8 more cycles
     $stop ;
