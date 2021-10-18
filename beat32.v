fdcbf0c7e675e60964ceeafbfe3ca841c5d31e1f
polys18 <75305203+polys18@users.noreply.github.com>
2021-10-18 11:58:49 -0700

    Update beat32.v

--- beat32.v
+++ beat32.v
@@ -8,22 +8,24 @@ module beat32 (
   reg [21:0] next_state;
   dffr #(22) counter(.clk (clock),.r (reset),.d (next_state), .q (state));
   
-  assign next_state = state + 1;
-  
   always @(*) begin
     case(state)
       22'd3125000: next_state = 0;
       default: next_state = state + 1;
+    endcase
+     if (reset) begin
+        next_state = 0;
+     end else begin
+        next_state = next_state;
+     end
   end
   
   always @(*) begin
     if (state == 3125000) begin
       assign count = 1;
     end else begin
-      assign out = 0;
+      assign count = 0;
     end
   end
-      
-  assign next_state = reset ? 0 : next_state;
  
 endmodule
