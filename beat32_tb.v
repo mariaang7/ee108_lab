4eb5cfb37398832e424aa7ec20490d8d1fe86066
polys18 <75305203+polys18@users.noreply.github.com>
2021-10-18 12:24:54 -0700

    Update beat32_tb.v

--- beat32_tb.v
+++ beat32_tb.v
@@ -1,4 +1,23 @@
 module beat32_tb ();
-
+  reg clock, reset, count;
+  beat32 dud (.clock(clock), .reset(reset), .count(count));
+  initial
+    forever
+      begin
+        #5 clock = 1 ; #5 clock = 0 ;
+      end
+  initial begin
+    #10 rst = 0 ;    // start w/o reset to showx state
+    #20 rst = 1 ;    // reset
+    #10 rst = 0 ;    // remove reset
+    #30
+    %display("expected: 0 ---> %d", count);
+    #20
+    %display("expected: 1 ---> %d", count);
+    #80.   // 8 more cycles
+    $stop ;
+  end
+endmodule
+ 
 
 endmodule
