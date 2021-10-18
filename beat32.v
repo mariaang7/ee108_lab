c48e8a6bd2d433d9896e2725de1e36548f451cd1
polys18 <75305203+polys18@users.noreply.github.com>
2021-10-18 13:20:11 -0700

    Update beat32.v

--- beat32.v
+++ beat32.v
@@ -4,15 +4,15 @@ module beat32 (
   output reg count
 );
   //3125000
-  reg [21:0] state;
+  wire [21:0] state;
   reg [21:0] next_state;
   dffr #(22) counter(.clk (clock),.r (reset),.d (next_state), .q (state));
   
   always @(*) begin
     case(state)
       //uncomment next line for testbench and comment original
-      //22'd3: next_state = 0;
-      22'd3125000: next_state = 0;
+      //22'd6: next_state = 0;
+      22'd3124999: next_state = 0;
       default: next_state = state + 1;
     endcase
      if (reset) begin
@@ -24,8 +24,8 @@ module beat32 (
   
   always @(*) begin
     //uncomment next line for testbench and comment original
-    //if (state == 3) begin
-    if (state == 3125000) begin
+    if (state == 0) begin
+    //if (state == 3125000) begin
       assign count = 1;
     end else begin
       assign count = 0;
