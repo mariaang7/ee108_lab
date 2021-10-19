048aeb43bb0c49a5efb768ace2c4c200f7552aa7
cate2002 <91645354+cate2002@users.noreply.github.com>
2021-10-18 20:37:58 -0700

    Update blinker.v

--- blinker.v
+++ blinker.v
@@ -1,20 +1,17 @@
 module blinker (input wire switch,
-                input wire out,
+                output wire out,
                 input wire clock,
                 input wire reset
 
 );
 
-  reg next_switch;
-  dffr #(1) blinker(.clk(clock), .r(reset), .d(next_switch), .q(out));
+  reg next_out;
+  dffr #(1) blinker(.clk(clock), .r(reset), .d(next_out), .q(out));
   
   always @(*) begin
     case (out) 
-      1'b0: next_switch = 1'b1;
-      1'b1: next_switch = 1'b0;
-    default: next_switch = 1'b0;
+      1'b1: next_out = switch ? 1'b0 : 1'b1;
+    default: next_out = switch ? 1'b1 : 1'b0;
     endcase 
   end
-  
-
 endmodule
