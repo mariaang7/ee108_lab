d6e9af15d90e1bfc26909eac9b6f4d108dcc1163
cate2002 <91645354+cate2002@users.noreply.github.com>
2021-10-20 13:23:57 -0700

    Update timner_2.v

--- timner_2.v
+++ timner_2.v
@@ -1,14 +1,14 @@
-module timer (module timer (input wire count_en,
+module timer (input wire count_en,
               input wire [8:0] load_value,
               input wire clock,
               input wire reset,
-              output reg out
+              output wire out
 
   );
   
   reg [8:0] next_state;
   wire [8:0] state; 
-  wire load;
+  reg load;
   reg next_out;
   
   dffre #(9) counter(.clk(clock), .r(reset), .en(count_en), .d(next_state), .q(state));
@@ -16,26 +16,28 @@ module timer (module timer (input wire count_en,
   
   always @(*) begin
     casex({reset, clock, load})
-      2'b1xx: next_state = {9{1'b0}};
-      2'b010: next_state = count_en ? state - 1 : state;
-      2'b011: next_state = count_en ? load_value : state;
+      3'b1xx: next_state = {9{1'b0}};
+      3'b010: next_state = count_en ? state - 1 : state;
+      3'b011: next_state = count_en ? load_value : state;
       default: next_state = state; 
     endcase
   end 
   
   always @(*) begin
     case(state) 
-      1'b0: load = 1; 
+      1'b0: load = ~reset ? 1'b1 : 1'b0; 
+      default: load = 1'b0;
     endcase
-    default: load = 0;
+    
   end 
   
   always @(*) begin 
     casex({reset, out}) 
-      1'b1x: next_out = 1'b0;
-      1'b01: next_out = 1'b0;
+      2'b1x: next_out = 1'b0;
+      2'b01: next_out = 1'b0;
+      default: next_out = next_out;
     endcase
-    default: next_out = next_out;
+    
   end 
-  
-);
+
+endmodule
