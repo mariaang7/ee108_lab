55403bc27dc32805b6ccb508313ed4382c9c182b
polys18 <75305203+polys18@users.noreply.github.com>
2021-10-19 19:12:54 -0700

    Update programmable_blinker.v

--- programmable_blinker.v
+++ programmable_blinker.v
@@ -1,5 +1,4 @@
 module programmable_blinker (
-  input wire shift_left, shift_right,
   input clock,
   input reset,
   output wire out
@@ -7,10 +6,14 @@ module programmable_blinker (
 
   wire [8:0] shifter_out;
   wire [8:0] load_value_timer;
+  wire count;
+  wire timer_out;
   
-  beat32 may (.clock(), .reset(), .count());
-  shifter jeff (.shift_left(shift_left), .shift_right(shift_right), .clk(clock), .rst(reset), .out(shifter_out));
-  timer joe (.load_value(load_value_timer), .clock(clock), .reset(reset), .out());
+  master
+  beat32 may (.clock(clock), .reset(reset), .count(count));
+  shifter jeff #()(.shift_left(shift_left), .shift_right(shift_right), .clk(clock), .rst(reset), .out(shifter_out));
+  timer joe (.count_en(count), .load_value(load_value_timer), .clock(clock), .reset(reset), .out());
+  blinker nin (.switch(timer_out), .clock(clock), .reset(reset), .out(out));
  
 
 endmodule
