e053ae1efe8362c17230931427e07077654229bb
mariaang7 <78777007+mariaang7@users.noreply.github.com>
2021-10-20 13:17:01 -0700

    Update timer_tb.v

--- timer_tb.v
+++ timer_tb.v
@@ -2,8 +2,6 @@ module timer_tb ();
   reg clock, reset, count_en;
   reg [8:0] load_value;
   wire out;
-  reg next_out;
-  reg [2:0] next_state, state;
   
   
     
@@ -12,7 +10,7 @@ module timer_tb ();
         forever
             begin 
                 #5 clock = 1; #5 clock = 0;
-                $display("%b %b %b %b %b %b %b %b %b %b", reset, clock, state, faster, slower, next, shift_left_1, shift_left_2, shift_right_1, shift_right_2);
+                //$display("%b %b %b %b %b %b %b %b %b %b", reset, clock, state, faster, slower, next, shift_left_1, shift_left_2, shift_right_1, shift_right_2);
             end
      initial begin
         #10
@@ -25,9 +23,7 @@ module timer_tb ();
          load_value = 001000000;
         #10 
          count_en = 1;
-        #100
-         load_value = 000010000;
-        #250
+        #300
          count_en = 0;
         #30
         $stop;
