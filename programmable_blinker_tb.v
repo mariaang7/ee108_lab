8585b2b3d2b9eaa06b927c1e5083ef96116797cc
mariaang7 <78777007+mariaang7@users.noreply.github.com>
2021-10-20 10:05:57 -0700

    Update programmable_blinker_tb.v

--- programmable_blinker_tb.v
+++ programmable_blinker_tb.v
@@ -1,3 +1,46 @@
 module programmable_blinker_tb ();
-
+  module programmable_blinker #(parameter FLASH = 1)(
+  reg shift_left1, shift_right1, shift_left2, shift_right2;
+  reg clock;
+  reg reset;
+  wire out;
+  
+    programmable binker #(0) dut1 (shift_left1, shift_right1, clk, rst);
+    programmable binker #(1) dut1 (shift_left2, shift_right2, clk, rst);
+    
+    initial 
+      forever
+        begin
+          #5 clock = 1; #5 clock = 0;
+          $display("%b %b %b %b %b %b", rst, shift_left1, shift_right1, shift_left2, shift_right2, out);
+        end
+    
+    initial begin
+      #10
+      rst = 0;
+      #10
+      rst = 1;
+      #10
+      rst = 0;
+      #10
+      shift_left1 = 1;
+      shift_right1 = 0;
+      #10
+      shift_left1 = 0;
+      shift_right1 = 0;
+      #10
+      rst = 1;
+      #10
+      rst = 0;
+      #10
+      shift_left2 = 1;
+      shift_right2 = 0;
+      #10
+      shift_left2 = 0;
+      shift_right2 = 0;
+      #40
+      
+      $stop;
+    end
+    
 endmodule
