3e7334877982b52301026d732b29bf44b9c097e8
cate2002 <91645354+cate2002@users.noreply.github.com>
2021-10-20 10:20:32 -0700

    Update timner_2.v

--- timner_2.v
+++ timner_2.v
@@ -7,55 +7,35 @@ module timer (module timer (input wire count_en,
   );
   
   reg [8:0] next_state;
-  wire state; 
-  wire [8:0] value;
+  wire [8:0] state; 
+  wire load;
   reg next_out;
   
   dffre #(9) counter(.clk(clock), .r(reset), .en(count_en), .d(next_state), .q(state));
   dffr #(1) states(.clk(clock), .r(reset), .d(next_out), .q(out));  
   
-  dffr #(1) states(.clk(clock), .r(reset), .d(next_out), .q(out));  
-  
   always @(*) begin
-    case (state) 
-      1'b0: next_state = count_en && (value = 0) ? 1'b1 : 1'b0; 
-      1'b1: next_state = count_en ? 1'b0 : 1'b1;
-      default: next_state = 1'b0;
-    endcase 
+    casex({reset, clock, load})
+      2'b1xx: next_state = {9{1'b0}};
+      2'b010: next_state = count_en ? state - 1 : state;
+      2'b011: next_state = count_en ? load_value : state;
+      default: next_state = state; 
+    endcase
   end 
   
   always @(*) begin
-    case (out) 
-      1'b0: next_out = count_en && (value = 0) ? 1'b1 : 1'b0; 
-      1'b1: next_out = count_en ? 1'b0 : 1'b1;
-      default: next_out = 1'b0;
-    endcase 
+    case(state) 
+      1'b0: load = 1; 
+    endcase
+    default: load = 0;
   end 
   
-  always @(*) begin
-    if (state == 1'b0) begin
-      value = value - 1;
-    end 
-    else if (state == 1'b1) begin
-      value = load_value;
-    end 
-    else begin
-      value = load_value;
-    end 
+  always @(*) begin 
+    casex({reset, out}) 
+      1'b1x: next_out = 1'b0;
+      1'b01: next_out = 1'b0;
+    endcase
+    default: next_out = next_out;
   end 
-  
-  always @(*) begin
-    if (out == 1'b0) begin
-      next_out = 1'b1;
-    end 
-    else if (out == 1'b0) begin
-      next_out = 1'b1;
-    end 
-    else begin
-      next_out = 1'b0;
-    end 
-  end 
-
-  
   
 );
