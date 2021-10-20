e3f14ab9ce6de5d0abcf40f3d8689c00b2380879
cate2002 <91645354+cate2002@users.noreply.github.com>
2021-10-20 00:15:25 -0700

    Update timer.v

--- timer.v
+++ timer.v
@@ -1,45 +1,40 @@
 module timer (input wire count_en,
-              input reg [8:0] load_value,
+              input wire [8:0] load_value,
               input wire clock,
               input wire reset,
               output reg out
 
-);
+  );
+  
   reg [8:0] next_state;
-  reg [8:0] state; 
+  wire state; 
+  wire [8:0] value = load_value - 1;
   reg next_out;
   
-  dffre #(x+1) counter(.clk(clock), .r(reset), .en(count_en), .d(next_state), .q(state));
-  dffr #() states(.clk(clock), .d(next_out), .q(out));  
-  
-  always @(*) begin
-    case(state) 
-      1'b0: next_state = count_en ? load_value : 1'b0;
-    default: next_state = count_en ? state - 1 : state;
-    endcase 
-  end 
-  
-  always @(*) begin
-    if (state == 1'b0) begin
-      case(out)
-        1'b0: next_out = 1'b1;
-      default: next_out = 1'b0;
-      endcase 
-    end 
-    else 
-      next_out = 1'b0;
-    end 
-  end 
+  dffre #(9) counter(.clk(clock), .r(reset), .en(count_en), .d(next_state), .q(state));
+  dffr #(1) states(.clk(clock), .r(reset), .d(next_out), .q(out));  
   
   
   always @(*) begin
     if (reset) begin
-      out = 1'b0;
-      next_state = load_value;
-      end 
-    else begin 
-      out = out;
-      end 
-  end
+      next_state = 0;
+    end
+    else 
+      case (state)
+        1'b0: next_state = count_en ? load_value : 1'b0; 
+              out = 1'b1; 
+        default: next_state = count_en ? state - 1 : state;
+              out = 1'b0;
+      endcase
+    
+  end 
+  
+  always @(*) begin
+    if (reset) begin
+      
+    end 
+  end 
+  
+  
 
 endmodule
