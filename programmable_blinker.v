d7f9b76e67ebbb06402e6d0a06099a67e654dc50
polys18 <75305203+polys18@users.noreply.github.com>
2021-10-19 19:30:19 -0700

    Update programmable_blinker.v

--- programmable_blinker.v
+++ programmable_blinker.v
@@ -10,7 +10,7 @@ module programmable_blinker (
   wire timer_out;
   
   beat32 may (.clock(clock), .reset(reset), .count(count));
-  shifter jeff #()(.shift_left(shift_left), .shift_right(shift_right), .clk(clock), .rst(reset), .out(shifter_out));
+  shifter #() jeff (.shift_left(shift_left), .shift_right(shift_right), .clk(clock), .rst(reset), .out(shifter_out));
   timer joe (.count_en(count), .load_value(shifter_out), .clock(clock), .reset(reset), .out());
   blinker nin (.switch(timer_out), .clock(clock), .reset(reset), .out(out));
  
