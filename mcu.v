5ca761e9f7561e7c853ac8f9eb5bcb81591be430
polys18 <75305203+polys18@users.noreply.github.com>
2021-10-23 17:46:14 -0700

    Update mcu.v

--- mcu.v
+++ mcu.v
@@ -9,39 +9,43 @@ module mcu(
     input song_done
 );
     
+    reg next_song;
+    reg next_play;
     
+    dffr #(22) counter(.clk (clk),.r (reset),.d (next_song), .q (song));
+    dffr #(22) counter(.clk (clk),.r (reset),.d (next_play), .q (play));
 
     always @(*) begin
         if (reset = 1) begin
-            play = 1'b0;
+            next_play = 1'b0;
             reset_player = 1'b1;
-            song = 2'b00;
+            next_song = 2'b00;
         end else begin
-            song = song;
+            next_song = song;
         end
     end
        
     always @(*) begin
         case (next_button) 
-            1'b1: {song, reset_player, play} = {(song == 2'b11) ? 0 : song + 1, 1'b1, 1'b0};
-            default song = song;
+            1'b1: {next_song, reset_player, next_play} = {(song == 2'b11) ? 0 : song + 1, 1'b1, 1'b0};
+            default next_song = song;
         endcase
     end
     
     always @(*) begin
         case (song_done) 
-            1'b1: {song, reset_player, play} = {(song == 2'b11) ? 0 : song + 1, 1'b1, 1'b0};
-            default song = song;
+            1'b1: {next_song, reset_player, next_play} = {(song == 2'b11) ? 0 : song + 1, 1'b1, 1'b0};
+            default next_song = song;
         endcase
     end
         
     always @(*) begin
         if (play_button == 1 && play == 0) begin
-            play = 0'b1;
+            next_play = 0'b1;
         end else if (play_button == 1 && play == 1) begin
-            play = 1'b0;
+            next_play = 1'b0;
         end else begin
-            play = play;
+            next_play = play;
         end
     end
 
