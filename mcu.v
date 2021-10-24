2adedd272865b5a7a107c215fe2d7fd5cfdadacd
polys18 <75305203+polys18@users.noreply.github.com>
2021-10-23 17:22:49 -0700

    Update mcu.v

--- mcu.v
+++ mcu.v
@@ -9,6 +9,38 @@ module mcu(
     input song_done
 );
 
-    // Implementation goes here!
+    always @(*) begin
+        if (reset = 1) begin
+            play = 1'b0;
+            reset_player = 1'b1;
+            song = 2'b00;
+        end else begin
+            song = song;
+        end
+    end
+       
+    always @(*) begin
+        case (next_button) 
+            1'b1: {song, reset_player, play} = {(song == 2'b11) ? 0 : song + 1, 1'b1, 1'b0};
+            default song = song;
+        endcase
+    end
+    
+    always @(*) begin
+        case (song_done) 
+            1'b1: {song, reset_player, play} = {(song == 2'b11) ? 0 : song + 1, 1'b1, 1'b0};
+            default song = song;
+        endcase
+    end
+        
+    always @(*) begin
+        if (play_button == 1 && play == 0) begin
+            play = 0'b1;
+        end else if (play_button == 1 && play == 1) begin
+            play = 1'b0;
+        end else begin
+            play = play;
+        end
+    end
 
 endmodule
