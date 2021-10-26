267ab1c48e92ec449e27323e39a993cef2c02969
polys18 <75305203+polys18@users.noreply.github.com>
2021-10-25 17:31:46 -0700

    Update note_player.v

--- note_player.v
+++ note_player.v
@@ -12,6 +12,23 @@ module note_player(
     output new_sample_ready  // Tells the codec when we've got a sample
 );
 
-    // Implementation goes here!
+    reg [5:0] count;
+    wire [19:0] frequency;
+    frequency_rom note (.clk(clk), .addr(note_to_load), .dout(frequency));
+    // frequency to be the input of the sine reader
+    
+    always @(*) begin
+        if (beat == 1'b1 && duration_to_load != count) begin
+            count += 1;
+        end else if (duration == count) begin
+            done_with_note = 1'b1;
+            count = 1'b0;
+        end else begin
+            done_with_note = 1'b0;
+        end
+    end
+    
+    
+         
 
 endmodule
