c3adb25741045c00aa2ded3045adb9f8d3f2dad9
mariaang7 <78777007+mariaang7@users.noreply.github.com>
2021-10-26 19:02:34 -0700

    Update sine_reader.v

--- sine_reader.v
+++ sine_reader.v
@@ -8,6 +8,21 @@ module sine_reader(
     output wire [15:0] sample
 );
 
-    //implementation goes here!
+    wire [15:0] out;
+    
+    reg [21:0] next_address;
+    wire [21:0] address;
+    dffr #(22) counter (.clk(clk), .r(reset), .d(next_address), .q(address));
+    
+    sine_rom sines(.clk(clk), .addr(address[19:10]), .dout(out));
+    
+    always @(*) begin 
+        case (generate_next)
+            1'b1: next_address = address + step_size;
+            default: next_address = address;
+        endcase
+    end 
+   
+            
 
 endmodule
