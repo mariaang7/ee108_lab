773e9f09b2d594205ae111cca53bb95c2a795d2e
cate2002 <91645354+cate2002@users.noreply.github.com>
2021-10-29 13:30:13 -0700

    Update wave_capture.v

--- wave_capture.v
+++ wave_capture.v
@@ -24,19 +24,28 @@ module wave_capture (
     dffr #(2) states(.clk(clk), .r(reset), .d(next_state), .q(state));
     dffr #(2) counter(.clk(clk), .r(reset), .d(next_count), .q(count));
     
+    ram_1w2r #(8, 8) ram(.clka(clk), .wea(write_enable), addra(), .dina(), .douta(), .clkb(clk), addrb(), doutb());
+    
     
     always @(*) begin
         case(state)
             2'b00: begin
                 next_state = (new_sample_in >= 0) ? 2'b01 : 2'b00;
+                next_count = 0;
             end 
             2'b01: begin
+                next_state = (count == 8'b11111111) ? 2'b10 : 2'b00;
+                next_count = (new_sample_ready) ? count + 1 : count;
             end 
             2'b10: begin
+                next_state = wave_display_idle ? 2'b00 : 2'b10;
+                //next_count = 0;
+                read_index = ~read_index;
             end 
         default: next_state = 2'b00;
         endcase 
     end 
     
+    always
     
 endmodule
