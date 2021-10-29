b2092d00f4b35854e221bec7862c6e8470ab4621
cate2002 <91645354+cate2002@users.noreply.github.com>
2021-10-29 14:36:55 -0700

    Update wave_capture_tb.v

--- wave_capture_tb.v
+++ wave_capture_tb.v
@@ -1,5 +1,45 @@
-module wave_capture_tb (
+module wave_capture_tb ();
 
+reg clk, reset, new_sample_ready, wave_display_idle;
+reg [15:0] new_sample_in;
+wire [8:0] write_address;
+wire write_enable;
+wire [7:0] write_sample;
+wire read_index;
+
+wave_capture dut(
+    .clk(clk), 
+    .reset(reset), 
+    .new_sample_ready(new_sample_ready), 
+    .new_sample_in(new_sample_in), 
+    .wave_display_idle(wave_display_idle), 
+    .write_address(write_address),
+    .write_enable(write_enable), 
+    .write_sample(write_sample),
+    .read_index(read_index)
 );
+
+
+    // Clock and reset
+    initial begin
+        clk = 1'b0;
+        reset = 1'b1;
+        repeat (4) #5 clk = ~clk;
+        reset = 1'b0;
+        forever #5 clk = ~clk;
+    end
+    
+    // Tests
+    initial begin
+        #10
+        new_sample_in = 16'b1111111111111111;
+        new_sample_ready = 1'b1;
+        #20
+        new_sample_ready = 1'b0;
+        #20
+        wave_display_idle = 1'b1;
+        #20
+        wave_display_idle = 1'b0; 
+    end
 
 endmodule
