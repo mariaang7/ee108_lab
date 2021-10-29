b19f5d2a5f4a31c94b85b7bdad5e5b569f6b2a2b
cate2002 <91645354+cate2002@users.noreply.github.com>
2021-10-29 11:21:14 -0700

    Update wave_capture.v

--- wave_capture.v
+++ wave_capture.v
@@ -10,9 +10,33 @@ module wave_capture (
     output wire [7:0] write_sample,
     output wire read_index
 );
-    `define STATE armed 
-    `define STATE active
-    `define STATE wait
+    `define STATE_ARMED 2'b00
+    `define STATE_ACTIVE 2'b01
+    `define STATE_WAIT 2'b10
+    
+    
+    reg [1:0] next_state;
+    wire [1:0] state;
+    wire [7:0] count;
+    reg [7:0] next_count;
+    
+    
+    dffr #(2) states(.clk(clk), .r(reset), .d(next_state), .q(state));
+    dffr #(2) counter(.clk(clk), .r(reset), .d(next_count), .q(count));
+    
+    
+    always @(*) begin
+        case(state)
+            2'b00: begin
+                next_state = (new_sample_in >= 0) ? 2'b01 : 2'b00;
+            end 
+            2'b01: begin
+            end 
+            2'b10: begin
+            end 
+        default: next_state = 2'b00;
+        endcase 
+    end 
     
     
 endmodule
