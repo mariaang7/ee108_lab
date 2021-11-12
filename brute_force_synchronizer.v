9bd5994f055221b1916ee28817b25ea7177f1b67
cate2002 <91645354+cate2002@users.noreply.github.com>
2021-11-11 20:40:36 -0800

    Add files via upload

--- brute_force_synchronizer.v
+++ brute_force_synchronizer.v
@@ -3,17 +3,17 @@
 //   Basic 3-flip-flop brute-force synchronizer.
 //   Inputs pass through a 3-ff chain and come out synchronized.
 //
-module brute_force_synchronizer(
+module brute_force_synchronizer #(parameter WIDTH=1)(
     input clk,  // Standard system clock.
-    input in,   // Async input 
-    output out  // Virtually guaranteed sync output.
+    input [WIDTH-1:0] in,   // Async input 
+    output [WIDTH-1:0] out  // Virtually guaranteed sync output.
 );
 
     // Our chain of three FFs.
     wire ff1_out, ff2_out;
 
-    dff ff1(.clk(clk), .d(in), .q(ff1_out));
-    dff ff2(.clk(clk), .d(ff1_out), .q(ff2_out));
-    dff ff3(.clk(clk), .d(ff2_out), .q(out));
+    dff #(WIDTH) ff1(.clk(clk), .d(in), .q(ff1_out));
+    dff #(WIDTH) ff2(.clk(clk), .d(ff1_out), .q(ff2_out));
+    dff #(WIDTH) ff3(.clk(clk), .d(ff2_out), .q(out));
 
 endmodule
