30ffcf74030c05549c3a5c9579ba2afb15c3441f
mariaang7 <78777007+mariaang7@users.noreply.github.com>
2021-11-27 11:58:05 -0800

    Update enhanced_waveform_display.v

--- enhanced_waveform_display.v
+++ enhanced_waveform_display.v
@@ -14,7 +14,10 @@ module wave_display (
 );
     
     reg valid_x;
+    wire [7:0] read_value_adjusted;
     
+    assign read_value_adjusted = (read_value >> 1) + 8'b01000000;
+    //assign read_value_adjusted = (read_value >> 1) + 8'b00100000;
     always @(*) begin
         case(x[10:8])
             3'b000: begin 
@@ -43,21 +46,24 @@ module wave_display (
     assign valid_pixel = (valid_x && ~y[9]);  //if pixel in quadrant 1 or 2
     
     wire [8:0] last_address;
-    ddfre #(9) addr_dff (.clk(clk), .r(reset), .en(valid), .d(read_address), .q(last_address));
+    dffre #(9) addr_dff (.clk(clk), .r(reset), .en(valid), .d(read_address), .q(last_address));
     
     wire en_read = ~(read_address == last_address);
    
     wire [7:0] prev_read_value;
-    dffr #(8) RAM_dffre (.clk(clk), .r(reset), en(en_read), .d(read_value), .q(prev_read_value));
+    dffre #(8) RAM_dffre(.clk(clk), .r(reset), .en(en_read), .d(read_value_adjusted), .q(prev_read_value));
     
-    wire [9:0] y_top = {1'b0, y[8:1]};  //y in top half
+    //wire [9:0] y_top = {1'b0, y[8:1]};  //y in top half
     
     wire magn_valid, magn_valid1, magn_valid2;
     
-    assign magn_valid1 = ((read_value >= y_top[8:1]) && (y_top[8:1] >= prev_read_value));
-    assign magn_valid2 = ((read_value =< y_top[8:1]) && (y_top[8:1] =< prev_read_value));
+    assign magn_valid1 = ((read_value_adjusted >= y[8:1]) && (y[8:1] >= prev_read_value));
+    assign magn_valid2 =((read_value_adjusted <= y[8:1]) && (y[8:1] <= prev_read_value));
     assign magn_valid = (magn_valid1 || magn_valid2);     //high if between RAM[x] and RAM[x-1]
     
-    assign {r,g,b} = (magn_valid)? 24'h0000f0 : 24'hffffff;
-           
+   
+    //assign {r,g,b} = (magn_valid)? 24'h0000f0 : 24'hffffff;
+    assign {r,g,b} = (magn_valid)? 24'hffffff : 24'h000000;
+            
+   
 endmodule
