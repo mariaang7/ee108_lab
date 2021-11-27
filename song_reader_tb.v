86255581dc9b5022dcecf49ea6aa0055e3cc9558
polys18 <75305203+polys18@users.noreply.github.com>
2021-11-27 15:59:34 -0800

    Update song_reader_tb.v

--- song_reader_tb.v
+++ song_reader_tb.v
@@ -2,22 +2,37 @@ module song_reader_tb();
 
     reg clk, reset, play, note_done;
     reg [1:0] song;
-    wire [5:0] note;
-    wire [5:0] duration;
-    wire song_done, new_note;
-
-    song_reader dut(
-        .clk(clk),
-        .reset(reset),
-        .play(play),
-        .song(song),
-        .song_done(song_done),
-        .note(note),
-        .duration(duration),
-        .new_note(new_note),
-        .note_done(note_done)
+    wire song_done;
+    wire done_with_note_one, done_with_note_two, done_with_note_three;
+    wire [5:0] note_one, note_two, note_three;
+    wire [5:0] duration_one, duration_two, duration_three;
+    wire new_note_one, new_note_two, new_note_three;
+    wire advance_done;
+    wire [5:0] advance_duration;
+    wire beat;
+    
+    song_reader dut (
+    .clk(clk),
+    .reset(reset),
+    .play(play),
+    .song(song),
+    .advance_done(advance_done),
+    .advance_duration(advance_duration),
+    .note_one_done(done_with_note_one),
+    .note_two_done(done_with_note_two),
+    .note_three_done(done_with_note_three),
+    .beat(beat),
+    .song_done(song_done),
+    .note_one(note_one),
+    .note_two(note_two),
+    .note_three(note_three),
+    .duration_one(duration_one),
+    .duration_two(duration_two),
+    .duration_three(duration_three),
+    .new_note_one(new_note_one),
+    .new_note_two(new_note_two),
+    .new_note_three(new_note_three)
     );
-
     // Clock and reset
     initial begin
         clk = 1'b0;
@@ -29,7 +44,9 @@ module song_reader_tb();
 
     // Tests
     initial begin
-
+        play = 1'b1;
+        song = 1'b1;
+        
     end
 
 endmodule
