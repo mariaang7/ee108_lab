fcf3943bed570b77ec14b18262a235da68c2783e
mariaang7 <78777007+mariaang7@users.noreply.github.com>
2021-12-05 19:59:39 -0800

    Update song_reader_tb_12.v

--- song_reader_tb_12.v
+++ song_reader_tb_12.v
@@ -1,66 +1,177 @@
-module song_reader_tb();
+`define SONG_WIDTH 7
+`define NOTE_WIDTH 6
+`define DURATION_WIDTH 6
 
-    reg clk, reset, play, note_done;
-    reg [1:0] song;
-    wire song_done;
-    wire [5:0] note_one, note_two, note_three;
-    wire [5:0] duration_one, duration_two, duration_three;
-    wire new_note_one, new_note_two, new_note_three;
-/*    wire advance_done;
-    wire [5:0] advance_duration;*/
-    wire beat;
-    reg note_one_done, note_two_done, note_three_done;
-    
-    //beat_generator beat_song_reader (.clk(clk), .reset(reset), .en(), .beat(beat));
+// ----------------------------------------------
+// Define State Assignments
+// ----------------------------------------------
+`define SWIDTH 3
+`define PAUSED             3'b000
+`define WAIT               3'b001
+`define INCREMENT_ADDRESS  3'b010
+`define RETRIEVE_NOTE      3'b011
+`define NEW_NOTE_READY     3'b100
 
-    song_reader dut (
-    .clk(clk),
-    .reset(reset),
-    .play(play),
-    .song(song),
-    .note_one_done(note_one_done),
-    .note_two_done(note_two_done),
-    .note_three_done(note_three_done),
-    .beat(beat),
-    .song_done(song_done),
-    .note_one(note_one),
-    .note_two(note_two),
-    .note_three(note_three),
-    .duration_one(duration_one),
-    .duration_two(duration_two),
-    .duration_three(duration_three),
-    .new_note_one(new_note_one),
-    .new_note_two(new_note_two),
-    .new_note_three(new_note_three)
+
+module song_reader(
+    input clk,
+    input reset,
+    input play,
+    input [1:0] song,
+    input note_one_done, note_two_done, note_three_done,
+    //input rewind,
+    //input ff,
+    input beat,
+    output song_done,
+    output  [5:0] note_one, note_two, note_three,
+    output  [5:0] duration_one, duration_two, duration_three,
+    output reg new_note_one, new_note_two, new_note_three
+);
+    wire [`SONG_WIDTH - 1:0] curr_note_num, next_note_num;
+    wire [15:0] note_and_duration;
+    wire [`SONG_WIDTH + 1:0] rom_addr = {song, curr_note_num - 1'b1};
+
+    wire [`SWIDTH-1:0] state;
+    reg  [`SWIDTH-1:0] next;
+
+    // For identifying when we reach the end of a song
+    wire overflow;
+
+    dffr #(`SONG_WIDTH) note_counter (
+        .clk(clk),
+        .r(reset),
+        .d(next_note_num),
+        .q(curr_note_num)
     );
     
-    // Clock and reset
-    initial begin
-        clk = 1'b0;
-        reset = 1'b1;
-        repeat (4) #5 clk = ~clk;
-        reset = 1'b0;
-        forever #5 clk = ~clk;
+    dffr #(`SWIDTH) fsm (
+        .clk(clk),
+        .r(reset),
+        .d(next),
+        .q(state)
+    );
+    
+    wire msb;
+    wire [5:0] note, duration;
+
+    song_rom rom(.clk(clk), .addr(rom_addr), .dout(note_and_duration));
+    assign {msb, note, duration} = {note_and_duration[15], note_and_duration[14:9], note_and_duration[8:3]};
+    
+  
+    wire advance_done;  
+    wire [5:0] state_t, next_state_t;
+    dffre #(6) advance_t (
+        .clk(clk),
+        .r(reset),
+        .en(msb && play),
+        .d(next_state_t),
+        .q(state_t)
+    );
+
+    assign next_state_t = (advance_done)
+                        ? duration : (beat ? state_t - 1 : state_t);
+
+    assign advance_done = (state_t == 6'b0);
+
+    reg increment;
+    
+    always @(*) begin
+        case (state)
+            `PAUSED:            next = play ? (msb ? `WAIT : `RETRIEVE_NOTE) : `PAUSED;
+            `RETRIEVE_NOTE:     next = play ? (msb ? `WAIT : `NEW_NOTE_READY) : `PAUSED;
+            `NEW_NOTE_READY:    next = play ? `WAIT : `PAUSED;
+            `WAIT:              next = ~play ? `PAUSED : (((increment && ~msb) || advance_done) ? `INCREMENT_ADDRESS
+                                                          : `WAIT);
+            `INCREMENT_ADDRESS: next = (play && ~overflow) ? `RETRIEVE_NOTE
+                                                           : `PAUSED;
+            default:            next = `PAUSED;
+        endcase
     end
     
-    initial begin
-        note_one_done = 1'b0;
-        note_two_done = 1'b0;
-        note_three_done = 1'b0;
-        forever #50 note_one_done = ~note_one_done;
-    end
-    initial begin
-        forever #100 note_two_done = ~note_two_done;
-    end
-    initial begin
-        forever #150 note_three_done = ~note_three_done;
-    end
+//    assign {overflow, next_note_num} =
+//        (state == `INCREMENT_ADDRESS) ? {1'b0, curr_note_num} + 1 : {1'b0, curr_note_num};]
 
-    // Tests
-    initial begin
-        play = 1'b1;
-        song = 1'b0;
-        #500;
+assign {overflow, next_note_num} =
+    (increment || advance_done) ? {1'b0, curr_note_num} + 1 : {1'b0, curr_note_num};
+        
+    wire new_note;
+    assign new_note = (state == `NEW_NOTE_READY);
+
+    assign song_done = overflow;
+    
+    reg note_one_busy, note_two_busy, note_three_busy;
+    
+//    wire [2:0] notes_done = {note_one_done, note_two_done, note_three_done};
+//    always @(*) begin
+//        casex({reset, notes_done})
+//            4'b01xx: note_one_busy = 1'b0;
+//            4'b0x1x: note_two_busy = 1'b0;
+//            4'b0xx1: note_three_busy = 1'b0;
+//            4'b1xxx: {note_one_busy, note_two_busy, note_three_busy} = 3'b0;
+//        endcase
+//     end
+    
+    wire [2:0] notes_done = {note_one_done, note_two_done, note_three_done};
+ 
+    reg [11:0] next_one, next_two, next_three;
+    wire [11:0] one, two, three;
+    
+    dffr#(12) dff_one (.clk(clk), .r(reset), .d(next_one), .q(one));
+    dffr#(12) dff_two (.clk(clk), .r(reset),.d(next_two), .q(two));
+    dffr#(12) dff_three (.clk(clk), .r(reset), .d(next_three), .q(three));
+    
+    always @(*) begin
+        casex(notes_done)
+            3'b1xx: 
+                begin
+                    next_one = {note, duration};
+                    new_note_one = 1'b1;
+                    increment = 1'b1;
+                    next_two = two;
+                    new_note_two = 1'b0;
+                    next_three = three;
+                    new_note_three = 1'b0;
+               end
+            3'b01x: 
+                begin
+                    next_two = {note, duration};
+                    new_note_two = 1'b1;
+                    increment = 1'b1;
+                    next_one = one;
+                    new_note_one = 1'b0;
+                    next_three = three;
+                    new_note_three = 1'b0;
+                end
+            3'b001:
+                begin 
+                    next_three = {note, duration};
+                    new_note_three = 1'b1;
+                    increment = 1'b1;
+                    next_two = two;
+                    new_note_two = 1'b0;
+                    next_one = one;
+                    new_note_one = 1'b0;
+                end
+            default: begin
+                next_one = one;
+                new_note_one = 1'b0;
+                next_two = two;
+                new_note_two = 1'b0;
+                next_three = three;
+                new_note_three = 1'b0;
+                increment = 1'b0;
+             end 
+        endcase
     end
+    
+    assign note_one = one[11:6];
+    assign duration_one = one[5:0];
+    assign note_two = two[11:6];
+    assign duration_two = two[5:0];
+    assign note_three = three[11:6];
+    assign duration_three=three[5:0];
+    
+
+    
 
 endmodule
