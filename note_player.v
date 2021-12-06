28cb9c7e31abf629b4de8caaac339670efb053c0
mariaang7 <78777007+mariaang7@users.noreply.github.com>
2021-12-05 20:00:24 -0800

    Update note_player.v

--- note_player.v
+++ note_player.v
@@ -5,17 +5,17 @@ module note_player(
     input [5:0] note_to_load,  // The note to play
     input [5:0] duration_to_load,  // The duration of the note to play
     input load_new_note,  // Tells us when we have a new note to load
-    input rewind,
+    //input rewind,
     output done_with_note,  // When we are done with the note this stays high.
     input beat,  // This is our 1/48th second beat
     input generate_next_sample,  // Tells us when the codec wants a new sample
     output [15:0] sample_out,  // Our sample output
     output new_sample_ready  // Tells the codec when we've got a sample
 );
-    //wire first_note;
+
     wire [19:0] step_size;
     wire [5:0] freq_rom_in;
-    //assign first_note = load_new_note;
+    
     dffre #(.WIDTH(6)) freq_reg (
         .clk(clk),
         .r(reset),
@@ -35,7 +35,7 @@ module note_player(
         .reset(reset),
         .step_size(step_size),
         .generate_next(play_enable && generate_next_sample),
-        .rewind(rewind),
+        //.rewind(rewind),
         .sample_ready(new_sample_ready),
         .sample(sample_out)
     );
@@ -49,15 +49,9 @@ module note_player(
         .q(state)
     );
     
-//    always @(*) begin
-//        if (!first_note) begin
-//            done_with_note = 1'b1;
-//        end else begin
-//            done_with_note = (state == 6'b0) && beat;
-//        end
-//    end
-    assign next_state = (reset || done_with_note || load_new_note)
-                        ? duration_to_load : state - 1;
+
+    assign next_state = (done_with_note || load_new_note)
+                        ? duration_to_load - 1 : state - 1;
 
     assign done_with_note = (state == 6'b0) && beat;
 
